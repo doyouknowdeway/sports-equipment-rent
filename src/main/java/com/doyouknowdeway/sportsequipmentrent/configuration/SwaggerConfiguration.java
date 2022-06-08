3ef6f30d05eead4719c6dd0174addc305d6bcd09
katseliv <katseliv@yandex.ru>
2022-06-08 09:23:16 +0300

    Added annotation @JsonDeserialize to Google Responses.

--- src/main/java/com/doyouknowdeway/sportsequipmentrent/configuration/SwaggerConfiguration.java
+++ src/main/java/com/doyouknowdeway/sportsequipmentrent/configuration/SwaggerConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -22,6 +24,8 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .host("sport-equipment-rent.herokuapp.com")
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(List.of(securityContext()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.doyouknowdeway.sportsequipmentrent.rest"))
                 .paths(PathSelectors.any())
@@ -37,6 +41,21 @@ public class SwaggerConfiguration {
                 .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
                 .version("1.0")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Bearer", authorizationScopes));
     }
 
 }
